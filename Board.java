87510b949a634fe85da1d2784a1fea552b501a3d
Eldar Kalachev <eldarkalachev@Eldars-MBP.Dlink>
2019-01-16 06:21:26 +0200

    Implemented simple part of Board object

--- Board.java
+++ Board.java
@@ -5,13 +5,29 @@
  **************************************************************************** */
 
 public class Board {
+    private final int[] pazzle;
+    private int dimensionLength;
+    private final int zero = 0;
+
     // construct a board from an n-by-n array of blocks
-    // (where blocks[i][j] = block in row i, column j)
     public Board(int[][] blocks) {
+        if (blocks == null)
+            throw new IllegalArgumentException("blocks object is NULL");
+
+        pazzle = new int[blocks.length * blocks[0].length];
+        dimensionLength = blocks.length;
+
+        int pazzleIndex = 0;
+        for (int[] row : blocks) {
+            for (int col : row) {
+                pazzle[pazzleIndex++] = col;
+            }
+        }
     }
 
     // board dimension n
     public int dimension() {
+        return dimensionLength;
     }
 
     // number of blocks out of place
@@ -24,6 +40,7 @@ public class Board {
 
     // is this board the goal board?
     public boolean isGoal() {
+        return hamming() == 0;
     }
 
     // a board that is obtained by exchanging any pair of blocks
@@ -32,6 +49,24 @@ public class Board {
 
     // does this board equal y?
     public boolean equals(Object y) {
+        if (y == null || this.getClass() != y.getClass())
+            return false;
+
+        Board that = (Board) y;
+
+        if (this.dimension() != that.dimension())
+            return false;
+
+        boolean areEquals = true;
+
+        for (int i = 0; i < this.pazzle.length; i++) {
+            if (this.pazzle[i] != that.pazzle[i]) {
+                areEquals = false;
+                break;
+            }
+        }
+
+        return areEquals;
     }
 
     // all neighboring boards
@@ -40,6 +75,17 @@ public class Board {
 
     // string representation of this board (in the output format specified below)
     public String toString() {
+        int index = 0;
+        String result = "";
+
+        for (int i = 0; i < this.dimension(); i++) {
+            for (int j = 0; j < this.dimension(); j++) {
+                result += pazzle[index++];
+            }
+            result += "\r\n";
+        }
+
+        return result;
     }
 
     // unit tests (not graded)
